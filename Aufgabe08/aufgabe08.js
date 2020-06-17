41097f229a2eef79bfbd7234add30b538ad8c99a
Janina Bach <janina.christina.petra.bach@hs-furtwangen.de>
2020-06-17 12:21:07 +0200

    ä

--- Aufgabe08/aufgabe08.js
+++ Aufgabe08/aufgabe08.js
@@ -17,7 +17,6 @@ var A08Server;
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
-        console.log(_request.url);
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
