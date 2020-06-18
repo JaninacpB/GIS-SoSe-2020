b20d31d54559a086cf74645e27e1f312a163c02d
Janina Bach <janina.christina.petra.bach@hs-furtwangen.de>
2020-06-18 20:24:52 +0200

    ddd

--- Aufgabe08/Artikel08.js
+++ Aufgabe08/Artikel08.js
@@ -2,22 +2,13 @@
 var Aufgabe08;
 (function (Aufgabe08) {
     let button = document.getElementById("SubmitButton");
-    let formData = new FormData(document.forms[0]);
     button.addEventListener("click", handlerButton);
-    function handlerButton() {
-        ausgabe();
-    }
-    async function ausgabe() {
+    async function handlerButton() {
+        let formData = new FormData(document.forms[0]);
         let url = "https://sosegis2020.herokuapp.com/";
         let query = new URLSearchParams(formData);
-        url = url + "?" + formData.toString();
-        let serverDaten = await fetch(url);
-        console.log(serverDaten);
-        for (let entry of query) {
-            console.log(entry);
-            console.log("name: " + entry[0]);
-            console.log("value: " + entry[1]);
-        }
+        url = url + "?" + query.toString();
+        console.log((await fetch(url)).url);
     }
 })(Aufgabe08 || (Aufgabe08 = {}));
 //# sourceMappingURL=Artikel08.js.map
--- Aufgabe08/aufgabe08.js
+++ Aufgabe08/aufgabe08.js
@@ -4,19 +4,23 @@ exports.A08Server = void 0;
 const Http = require("http");
 var A08Server;
 (function (A08Server) {
+    //Bestätigung, dass der Code läuft
     console.log("Starting server");
     let port = Number(process.env.PORT);
     if (!port)
         port = 8100;
+    // Listener, reagiert wenn etwas ankommt  
     let server = Http.createServer();
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
+    // Handler für Listener, schreibt in Console
     function handleListen() {
         console.log("Listening");
     }
     function handleRequest(_request, _response) {
         console.log(_request.url);
+        console.log(_response);
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
