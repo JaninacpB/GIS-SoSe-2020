b2e5aa1756036f8b4dcb3875704954d6846f7647
Janina Bach <janina.christina.petra.bach@hs-furtwangen.de>
2020-06-18 20:24:52 +0200

    ddd

--- Aufgabe08/Artikel08.ts
+++ Aufgabe08/Artikel08.ts
@@ -1,29 +1,17 @@
 namespace Aufgabe08 {
 
     let button: HTMLButtonElement = <HTMLButtonElement> document.getElementById("SubmitButton");
-    let formData: FormData = new FormData(document.forms[0]);
 
     button.addEventListener("click", handlerButton);
 
+    async function handlerButton(): Promise<void> { 
 
-    function handlerButton(): void {
-        ausgabe();
-    }
-
-    async function ausgabe(): Promise<void> { 
-
-    let url: string = "https://sosegis2020.herokuapp.com/";
-    let query: URLSearchParams = new URLSearchParams(<any>formData);
-    url = url + "?" + formData.toString();
-    let serverDaten: any = await fetch(url);
-    console.log(serverDaten);
-
-    for (let entry of query) {
-        console.log(entry);
-        console.log("name: " + entry[0]);
-        console.log("value: " + entry[1]);
-    }
-
+        let formData: FormData = new FormData(document.forms[0]);
+        let url: string = "https://sosegis2020.herokuapp.com/";
+        let query: URLSearchParams = new URLSearchParams(<any>formData);
+        url = url + "?" + query.toString();
+    
+        console.log((await fetch(url)).url);
     }
 
 }
--- Aufgabe08/aufgabe08.ts
+++ Aufgabe08/aufgabe08.ts
@@ -1,22 +1,27 @@
 import * as Http from "http";
 
 export namespace A08Server {
+  //Bestätigung, dass der Code läuft
   console.log("Starting server");
   let port: number = Number(process.env.PORT);
   if (!port)
     port = 8100;
 
+  
+  // Listener, reagiert wenn etwas ankommt  
   let server: Http.Server = Http.createServer();
   server.addListener("request", handleRequest);
   server.addListener("listening", handleListen);
   server.listen(port);
 
+  // Handler für Listener, schreibt in Console
   function handleListen(): void {
     console.log("Listening");
   }
 
   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
     console.log(_request.url);
+    console.log(_response);
 
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
