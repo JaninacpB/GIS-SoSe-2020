8b0927582ea843b0c5fc1165ad3b720165ac54c1
Janina Bach <janina.christina.petra.bach@hs-furtwangen.de>
2020-06-18 20:32:11 +0200

    r

--- Aufgabe08/aufgabe08.js
+++ Aufgabe08/aufgabe08.js
@@ -20,7 +20,6 @@ var A08Server;
     }
     function handleRequest(_request, _response) {
         console.log(_request.url);
-        console.log(_response);
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
